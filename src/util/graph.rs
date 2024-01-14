dc57ab8484e072d9d3814a83746d752977b6ef9f
ghostminer <1696415745@qq.com>
2024-01-15 01:34:52 +0800

    Get

--- src/util/graph.rs
+++ src/util/graph.rs
@@ -1,21 +1,7 @@
 mod raw {
     use std::io::{self, Error, ErrorKind};
 
-    use sqlx::{MySqlConnection, Row};
-
-    pub async fn get_subject(
-        conn: &mut MySqlConnection,
-        predicate: &str,
-        object: &str,
-    ) -> io::Result<String> {
-        let row = sqlx::query("select subject from edge_t where predicate=? and object=?")
-            .bind(predicate)
-            .bind(object)
-            .fetch_one(conn)
-            .await
-            .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
-        Ok(row.get(0))
-    }
+    use sqlx::MySqlConnection;
 
     pub async fn delete_predicate(
         conn: &mut MySqlConnection,
@@ -32,6 +18,53 @@ mod raw {
             .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
         Ok(())
     }
+
+    #[async_recursion::async_recursion]
+    pub async fn get(conn: &mut MySqlConnection, root: &str, path: &str) -> io::Result<String> {
+        if path.starts_with("->") || path.starts_with("<-") {
+            let (arrow, path) = (&path[0..2], &path[2..]);
+            let _v = path.find("->");
+            let v_ = path.find("<-");
+            if _v.is_some() || v_.is_some() {
+                let pos = if _v.is_some() && v_.is_some() {
+                    std::cmp::min(_v.unwrap(), v_.unwrap())
+                } else if _v.is_some() {
+                    _v.unwrap()
+                } else {
+                    v_.unwrap()
+                };
+                let predicate = &path[0..pos];
+                let path = &path[pos..];
+
+                let pt = if arrow == "->" {
+                    super::get_object(conn, root, predicate).await?
+                } else {
+                    super::get_subject(conn, predicate, root).await?
+                };
+                get(conn, &pt, path).await
+            } else {
+                if arrow == "->" {
+                    super::get_object(conn, root, path).await
+                } else {
+                    super::get_subject(conn, path, root).await
+                }
+            }
+        } else {
+            let _v = path.find("->");
+            let v_ = path.find("<-");
+            let pos = if _v.is_some() && v_.is_some() {
+                std::cmp::min(_v.unwrap(), v_.unwrap())
+            } else if _v.is_some() {
+                _v.unwrap()
+            } else {
+                v_.unwrap()
+            };
+            let root = &path[0..pos];
+            let path = &path[pos..];
+
+            get(conn, root, path).await
+        }
+    }
 }
 
 use std::io::{self, Error, ErrorKind};
@@ -43,50 +76,13 @@ pub fn new_point() -> String {
     uuid::Uuid::new_v4().to_string()
 }
 
-#[async_recursion::async_recursion]
 pub async fn get(conn: &mut MySqlConnection, root: &str, path: &str) -> io::Result<String> {
-    if path.starts_with("->") || path.starts_with("<-") {
-        let (arrow, path) = (&path[0..2], &path[2..]);
-        let _v = path.find("->");
-        let v_ = path.find("<-");
-        if _v.is_some() || v_.is_some() {
-            let pos = if _v.is_some() && v_.is_some() {
-                std::cmp::min(_v.unwrap(), v_.unwrap())
-            } else if _v.is_some() {
-                _v.unwrap()
-            } else {
-                v_.unwrap()
-            };
-            let predicate = &path[0..pos];
-            let path = &path[pos..];
-
-            let pt = if arrow == "->" {
-                get_object_anyway(conn, root, predicate).await?
-            } else {
-                get_subject_anyway(conn, predicate, root).await?
-            };
-            get(conn, &pt, path).await
-        } else {
-            if arrow == "->" {
-                get_object_anyway(conn, root, path).await
-            } else {
-                get_subject_anyway(conn, path, root).await
-            }
-        }
-    } else {
-        let _v = path.find("->");
-        let v_ = path.find("<-");
-        let pos = if _v.is_some() && v_.is_some() {
-            std::cmp::min(_v.unwrap(), v_.unwrap())
-        } else if _v.is_some() {
-            _v.unwrap()
-        } else {
-            v_.unwrap()
-        };
-        let root = &path[0..pos];
-        let path = &path[pos..];
-
-        get(conn, root, path).await
+    match raw::get(conn, root, path).await {
+        Ok(r) => Ok(r),
+        Err(e) => match e.kind() {
+            io::ErrorKind::NotFound => Ok(String::new()),
+            _ => Err(e),
+        },
     }
 }
 
@@ -122,10 +118,11 @@ pub async fn get_object(
         .bind(predicate)
         .fetch_one(conn)
         .await
-        .map_err(|e| Error::new(ErrorKind::Other, e.to_string()))?;
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => Error::new(ErrorKind::NotFound, e),
+            _ => Error::new(ErrorKind::Other, e),
+        })?;
     let object = row.get(0);
-    log::debug!("get_target: {subject}->{predicate}={object}");
-
     Ok(object)
 }
 
@@ -144,12 +141,29 @@ pub async fn get_object_anyway(
     }
 }
 
+pub async fn get_subject(
+    conn: &mut MySqlConnection,
+    predicate: &str,
+    object: &str,
+) -> io::Result<String> {
+    let row = sqlx::query("select subject from edge_t where predicate=? and object=?")
+        .bind(predicate)
+        .bind(object)
+        .fetch_one(conn)
+        .await
+        .map_err(|e| match e {
+            sqlx::Error::RowNotFound => Error::new(ErrorKind::NotFound, e),
+            _ => Error::new(ErrorKind::Other, e),
+        })?;
+    Ok(row.get(0))
+}
+
 pub async fn get_subject_anyway(
     conn: &mut MySqlConnection,
     predicate: &str,
     object: &str,
 ) -> io::Result<String> {
-    match raw::get_subject(conn, predicate, object).await {
+    match get_subject(conn, predicate, object).await {
         Ok(subject) => Ok(subject),
         Err(_) => {
             let subject = new_point();
